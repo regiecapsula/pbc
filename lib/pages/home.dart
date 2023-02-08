2da06252e3b47afe19992e966785f314cfcab4c8
regiecapsula <capsularegieph@gmail.com>
2023-02-08 22:28:50 +0800

    Read

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,22 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import '../items/item_announcement.dart';
 import 'package:firebase_core/firebase_core.dart';
 import '../firebase_options.dart';
 
 class Home extends StatelessWidget {
-
   final List listAnnouncements = ['post 1', 'post 2', 'post 3', 'post 4'];
+  String data ="";
+
+  getString() async {
+    FirebaseDatabase.instance
+        .ref("prayer_items/0/item")
+        .onValue
+        .listen((DatabaseEvent event) {
+      data = event.snapshot.value.toString();
+      print(data);
+    });
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -15,9 +26,12 @@ class Home extends StatelessWidget {
       body: ListView.builder(
         itemCount: listAnnouncements.length,
         itemBuilder: (context, index) {
+          getString();
           return Announcement(
-            announcement: listAnnouncements[index],
-          );
+              announcement: listAnnouncements[index]);
         },
       ));
+
+
+
 }
