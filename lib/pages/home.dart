3933d7778c2b371f7beda71581fac16a5dee9ccf
PBC03 <capsularegieph@gmail.com>
2023-02-12 16:31:33 +0800

    FirebaseAnimatedList

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,37 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import '../items/item_announcement.dart';
-import 'package:firebase_core/firebase_core.dart';
-import '../firebase_options.dart';
 
 class Home extends StatelessWidget {
   final List listAnnouncements = ['post 1', 'post 2', 'post 3', 'post 4'];
-  String data ="";
-
-  getString() async {
-    FirebaseDatabase.instance
-        .ref("prayer_items/0/item")
-        .onValue
-        .listen((DatabaseEvent event) {
-      data = event.snapshot.value.toString();
-      print(data);
-    });
-  }
+  final DatabaseReference dbRef = FirebaseDatabase.instance.ref("events/upcoming/2022");
 
   @override
   Widget build(BuildContext context) => Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: ListView.builder(
-        itemCount: listAnnouncements.length,
-        itemBuilder: (context, index) {
-          getString();
-          return Announcement(
-              announcement: listAnnouncements[index]);
-        },
+      body: Column(
+        children: [
+          Expanded(
+            child: FirebaseAnimatedList(
+                query: dbRef,
+                itemBuilder: (context, snapshot, animation, index){
+                  return ListTile(
+                    title: Text(snapshot.child('title').value.toString()),
+                    subtitle: Text(snapshot.child('details').value.toString()),
+                  );
+                }
+            ),
+          )
+        ],
       ));
-
-
 
 }
